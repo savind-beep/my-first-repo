d7b60cf0eb76bdc87b50d2342e95233c0f240e10
siriwardhana123 <savindusiriwardhanaa@gmail.com>
2026-06-03 01:12:49 +0530

    lab sheet 3

--- lab_sheet.py
+++ lab_sheet.py
@@ -16,3 +16,75 @@ GS=BC+OT+B
 TD=GS*(T/100)
 NS=GS-TD
 print("The net salary is:",NS)
+B=int(input("Enter the second number:"))
+C=0
+print=(A,B)
+C=A
+B=A
+C=B
+print=(A,B)'''
+
+'''A=int(input("Enter the first number:"))
+B=int(input("Enter the second number:"))
+if A>B:
+    print("The biggest number is:",A)   
+else :
+    print("The biggest number is:",B)'''
+
+'''Num1=int(input("Enter the first number:"))
+if Num1>=0:
+    print("The number is positive.")
+else:
+    print("The number is negative.")'''
+
+''''SM=int(input("Enter the marks:"))
+if SM>=50:
+    print("Passed")
+else:   
+     print("Failed")'''
+
+'''AGE=int(input("Enter the age:"))
+if AGE>=18:
+    print("You are eligible to vote.")  
+else:    print("You are not eligible to vote.")'''
+
+'''Num1=int(input("Enter the first number:"))
+if Num1%2==0:
+    print("The number is even.")        
+else:    print("The number is odd.") '''
+'''NUM1=int(input("Enter the first number:"))
+if NUM1>0:
+    print("The number is positive.")        
+elif NUM1<0:    print("The number is negative.") 
+else:    print("The number is zero.")'''
+
+'''SM=int(input("Enter the marks:"))
+if 90<=SM<=100:      
+    print("Grade A")
+elif 88<=SM<=89:
+    print("Grade B")
+elif 70<=SM<=79:
+    print("Grade C")
+elif 60<=SM<=69:
+    print("Grade D")        
+elif 50<=SM<=59:
+    print("Grade E") 
+else:    print("Grade F")'''
+
+'''FN=int(input("Enter the first number:"))
+SN=int(input("Enter the second number:"))
+OP=input("Enter the operator (+, -, *, /):")    
+if OP=='+':
+    result=FN+SN
+    print("The result is:",result)
+elif OP=='-':
+    result=FN-SN
+    print("The result is:",result)
+elif OP=='*':
+    result=FN*SN
+    print("The result is:",result)
+elif OP=='/':
+    result=FN/SN
+    print("The result is:",result)
+else:
+    print("Invalid operator") '''  
